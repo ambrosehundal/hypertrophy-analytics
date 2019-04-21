c4f1d51c4064d9b42365402d90d97629a9314274
Ambrose Hundal <amshunda@ucsc.edu>
2019-04-21 11:56:43 -0700

    workout controller added

--- app/controllers/workouts_controller.rb
+++ app/controllers/workouts_controller.rb
@@ -1,2 +1,20 @@
 class WorkoutsController < ApplicationController
+
+    def index
+        @workouts = Workout.all 
+    end 
+
+    def new
+        @workout = Workout.new
+    end
+
+    def show
+        @workout = Workout.find(params[:id])
+    end
+
+    private
+
+    def workout_params
+        params.require(:workout).permit(:name, :workout_date, :cardio)
+    end
 end

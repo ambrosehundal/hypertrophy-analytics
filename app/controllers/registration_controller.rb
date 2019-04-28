e9e15dd9c5e3066c3740a229fc99b35bffc48bc4
Ambrose Hundal <amshunda@ucsc.edu>
2019-04-27 18:38:57 -0700

    configured parameters for new user registration

--- app/controllers/registration_controller.rb
+++ app/controllers/registration_controller.rb
@@ -1,9 +1,13 @@
 class RegistrationController < Devise::RegistrationController
-    prepend_before_action :require_no_authentication, only: :cancel
+   # prepend_before_action :require_no_authentication, only: :cancel
     protected
 
     def after_sign_in_path_for(users)
         workout_path
+    end
+
+    def create
+    
     end
 
 

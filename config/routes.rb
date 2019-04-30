c60861c588138213651e772665be2b80078bfe7e
Ambrose Hundal <amshunda@ucsc.edu>
2019-04-29 21:49:44 -0700

    devise views editing

--- config/routes.rb
+++ config/routes.rb
@@ -5,8 +5,6 @@ Rails.application.routes.draw do
     resources :workouts
   end
 
-  devise_scope :user do
-    root to: "users/sessions#new"
-  end
+ 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end

d8f4fd7b7ae1ff372e4d0668c4029edc533e23ee
Ambrose Hundal <amshunda@ucsc.edu>
2019-04-29 23:50:53 -0700

    routing error fixed

--- config/routes.rb
+++ config/routes.rb
@@ -2,12 +2,12 @@ Rails.application.routes.draw do
   devise_for :users
 
   resources :users do
-    resources :workouts
+    resources :workouts do
+      resources :exercises
+    end
   end
 
-  resources :workouts do
-    resources :exercises
-  end
+
 
  
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html

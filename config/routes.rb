34676ae3b6170bf4190e1f86dce4aec4f04db9b6
Ambrose Hundal <amshunda@ucsc.edu>
2019-05-02 23:57:40 -0700

    exercise model fixed

--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,11 @@ Rails.application.routes.draw do
   devise_for :users
 
   resources :users do
-    resources :workouts do
-      resources :exercises
-    end
+    resources :workouts 
+  end
+
+  resources :workouts do
+    resources :exercises
   end
 
   post '/users/:user_id/workouts/:workout_id/exercises' => 'exercises#create'

ecf0f117b20b3f004e3afabdb3bc1761ae8f035b
Ambrose Hundal <amshunda@ucsc.edu>
2019-05-03 12:28:05 -0700

    change exercise view route, controller works fine

--- app/controllers/exercises_controller.rb
+++ app/controllers/exercises_controller.rb
@@ -1,23 +1,23 @@
 class ExercisesController < ApplicationController
 
     def index
-        @user = User.find(params[:user_id])
-        @workout = @user.workouts.find(params[:workout_id])
+       # @user = User.find(params[:user_id])
+        @workout = Workout.find(params[:workout_id])
         @exercises = @workout.exercises.all
         
 
     end 
 
     def new
-        @user = User.find(params[:user_id])
-        @workout = @user.workouts.find(params[:workout_id])
+      #  @user = User.find(params[:userid])
+        @workout = Workout.find(params[:workout_id])
         @exercise = @workout.exercises.new
        
     end
 
     def create
-        @user = User.find(params[:user_id])
-        @workout = @user.workouts.find(params[:workout_id])
+      #  @user = User.find(params[:user_id])
+        @workout = Workout.find(params[:workout_id])
         @exercise = @workout.exercises.create(exercise_params)
         #@workout.save
         redirect_to :action => 'index'
